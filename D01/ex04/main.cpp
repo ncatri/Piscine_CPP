7c06611a9b195a4a97e072ab9bad5d7ae80ab7a9
Nicolas Catriens <nicolas.catriens@gmail.com>
2021-11-23 13:46:53 +0100

    D01 ex04 seems ok. Maybe needs more tests

--- D01/ex04/main.cpp
+++ D01/ex04/main.cpp
@@ -1,15 +1,52 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+
+std::string	my_replace(std::string word, size_t pos, std::string search, std::string replace);
 
 int main(int argc, char **argv) {
 
-	(void)argc;
-	(void)argv;
-	std::ifstream ifs("numbers");
-	int	dst;
-	int	dst2;
-	ifs >> dst >> dst2;
-	std::cout << dst << " " << dst2 << std::endl;
-	ifs.close();
+	if (argc != 4) {
+		std::cerr << "Usage: ./replace <filename> <str_to_search> <str_to_replace>" << std::endl;
+		return (1);
+	}
+	std::string filename = argv[1];
+	std::ifstream ifs(filename);
+	std::ofstream ofs(filename + ".replace");
+	if (!ifs.good()) {
+		std::cerr << "Error: invalid file" << std::endl;
+		return (2);
+	} else if (!ofs.good()) {
+		std::cerr << "Error: can't create output file" << std::endl;
+		return (3);
+	}
+	std::string to_replace = argv[3];
+	std::string to_search = argv[2];
+	for (std::string line; std::getline(ifs, line); ) {
+		std::istringstream iss(line);
+		do {
+			std::string word;
+			iss >> word;
+
+			size_t pos = word.find(argv[2]);
+			if (pos == std::string::npos)
+				ofs << word;
+			else { 
+				word = my_replace(word, pos, to_search, to_replace);
+				ofs << word;
+			}
+			if (iss.peek() != -1)
+				ofs << ' ';
+		} while (iss);
+		ofs << '\n';
+	}
+	ifs.close();	
+	ofs.close();
 }
 
+std::string	my_replace(std::string word, size_t pos, std::string search, std::string replace) {
+
+	std::string prefix = word.substr(0, pos);
+	std::string suffix = word.substr(pos + search.size());
+	return (prefix + replace + suffix);
+}
