25021df704934038294b8c041d4402404356cb5e
Nicolas Catriens <nicolas.catriens@gmail.com>
2021-12-03 13:46:16 +0100

    D04 ready for correc

--- D04/ex01/Cat.hpp
+++ D04/ex01/Cat.hpp
@@ -8,7 +8,7 @@ class Cat : public Animal {
 	public:
 		Cat( void );
 		Cat( Cat const& src );
-		~Cat( );
+		virtual ~Cat( );
 		Cat&	operator=( Cat const& rhs );
 
 		void 	makeSound( void ) const;
--- D04/ex01/Dog.hpp
+++ D04/ex01/Dog.hpp
@@ -8,7 +8,7 @@ class Dog : public Animal {
 	public:
 		Dog( void );
 		Dog( Dog const& src );
-		~Dog( void );
+		virtual ~Dog( void );
 		Dog&	operator=( Dog const& rhs );
 
 		void	makeSound( void ) const;
