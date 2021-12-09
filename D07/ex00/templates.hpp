4b1b7a6458e1e761ad7daa494c1da991f7b00f37
Nicolas Catriens <nicolas.catriens@gmail.com>
2021-12-09 13:36:58 +0100

    D07 ex00 done?

---
+++ D07/ex00/templates.hpp
@@ -0,0 +1,13 @@
+#ifndef TEMPLATES_HPP
+#	define TEMPLATES_HPP
+
+template <typename T>
+void	swap(T *a, T *b);
+
+template <typename T>
+T	min(T& a, T& b);
+
+template <typename T>
+T	max(T& a, T& b);
+
+#endif
