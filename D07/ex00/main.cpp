7580362acce9935cc0a135e26849dc91f61103a5
Nicolas Catriens <nicolas.catriens@gmail.com>
2021-12-13 13:02:26 +0100

    D07 ex00 better implementing swap with references instead of pointers

--- D07/ex00/main.cpp
+++ D07/ex00/main.cpp
@@ -9,14 +9,14 @@ int main(void) {
 	Fixed b = Fixed(2);
 
 	std::cout << "a: " << a << " , b: " << b << std::endl;
-	swap<>(&a, &b);
+	swap<>(a, b);
 	std::cout << "a: " << a << " , b: " << b << std::endl;
 
 	std::string str1 = "salut";
 	std::string str2 = "goodbye";
 
 	std::cout << "str1: " << str1 << " , str2: " << str2 << std::endl;
-	swap<>(&str1, &str2);
+	swap<>(str1, str2);
 	std::cout << "str1: " << str1 << " , str2: " << str2 << std::endl;
 
 	std::cout << "--- test min() ---" << std::endl;
@@ -28,23 +28,4 @@ int main(void) {
 	std::cout << "max(a, b): " << max<>(a, b) << std::endl;
 	std::cout << "max(b, a): " << max<>(b, a) << std::endl;
 	std::cout << "max(str1, str2): " << max<>(str1, str2) << std::endl;
-}
-
-template <typename T>
-void	swap(T *a, T *b) {
-	T tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-template <typename T>
-T	min(T& a, T& b) {
-
-	return ( (a < b) ? a : b );
-}
-
-template <typename T>
-T	max(T& a, T& b) {
-
-	return ( (a > b) ? a : b );
 }
--- D07/ex00/templates.hpp
+++ D07/ex00/templates.hpp
@@ -2,12 +2,20 @@
 #	define TEMPLATES_HPP
 
 template <typename T>
-void	swap(T *a, T *b);
+void	swap(T& a, T& b) {
+	T	tmp = a;
+	a = b;
+	b = tmp;
+}
 
 template <typename T>
-T	min(T& a, T& b);
+T	min(T& a, T& b) {
+	return ( (a < b) ? a : b );
+}
 
 template <typename T>
-T	max(T& a, T& b);
+T	max(T& a, T& b) {
+	return ( (a > b) ? a : b );
+}
 
 #endif
